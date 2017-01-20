cc41005cbe8c45180dbec3fbef3f07490a01b949
Kyle Eisenbarger <keisenb@ksu.edu>
2017-01-19 23:23:03 -0600

    Added attendance metrics to overview page

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -61,4 +61,43 @@
       </div>
     </div>
   </div>
+
+  <div class="hr-divider m-t-md m-b">
+    <h3 class="hr-divider-content hr-divider-heading">Attendance</h3>
+  </div>
+
+  <div class="row statcards">
+    <div class="col-sm-6 col-lg-3 m-b">
+      <div class="statcard statcard-success">
+        <div class="p-a">
+          <span class="statcard-desc">Average Officers</span>
+          <h2 class="statcard-number">{{ $pizzaTotals->events }}%</h2>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-3 m-b">
+      <div class="statcard statcard-info">
+        <div class="p-a">
+          <span class="statcard-desc">First Place</span>
+          <h2 class="statcard-number">{{ $pizzaTotals->pizzasOrdered }}</h2>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-3 m-b">
+      <div class="statcard statcard-warning">
+        <div class="p-a">
+          <span class="statcard-desc">Second Place</span>
+          <h2 class="statcard-number">{{ ceil($pizzaTotals->avgSlicesPerPerson) }}</h2>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-lg-3 m-b">
+      <div class="statcard statcard-danger">
+        <div class="p-a">
+          <span class="statcard-desc">Last Place</span>
+          <h2 class="statcard-number">{{ ceil($pizzaTotals->avgLeftoverSlices) }}</h2>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
