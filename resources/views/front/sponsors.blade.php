c6adb5cc947c32ba6ec9a110896639ad2a3dbc6f
Kyle Eisenbarger <keisenb@ksu.edu>
2017-03-01 13:41:10 -0600

    Added sponsors to the sponsor page and made responsive

--- resources/views/front/sponsors.blade.php
+++ resources/views/front/sponsors.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.front')
 @section('title', 'Sponsors')
 
+@section('head')
+<link href="{{ URL::asset('css/sponsor.css') }}" media="all" rel="stylesheet" type="text/css">
+
+@endsection
 @section('content')
 
 <div class="container">
-  <div class="section">
-    <div class="row">
       <h1 class="header center grey-text text-darken-3">ACM Sponsors</h1>
     </div>
 
-    <div class="row">
-        <div class="col s12">
-            <div class="center promo">
-              <img src="{{ URL::asset('img/sponsors/jetbrains.png') }}" alt="">
-            </div>
-        </div>
-    </div>
+<section>
+  <div class="center">
+    <div class="sponsor">
+      <img src="{{ URL::asset('img/sponsors/jetbrains.png') }}" alt="">
+      <img src="{{ URL::asset('img/sponsors/bats.png') }}" alt="">
+      <img src="{{ URL::asset('img/sponsors/tech.png') }}" alt="">
+      <img src="{{ URL::asset('img/sponsors/garmin.png') }}" alt="">
   </div>
 </div>
+</section>
 
 @endsection
