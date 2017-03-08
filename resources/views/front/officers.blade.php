0f2064a3f2e5aa9f3003e3cb80eca9a8cd9763f2
Kyle Eisenbarger <keisenb@ksu.edu>
2017-03-08 14:59:53 -0600

    Started officer and JO page

--- resources/views/front/officers.blade.php
+++ resources/views/front/officers.blade.php
@@ -1,43 +1,166 @@
 @extends('layouts.front')
 @section('title', 'Officers')
 
-@section('content')
-<div class="container">
-  <div class="section">
-    <div class="row">
-      <h1 class="header center grey-text text-darken-3">ACM Officers</h1>
-    </div>
-    <div class="row">
-      <div class="col s12 m9 l10">
-        @foreach($officers as $officer)
-        <div class="card hoverable section scrollspy" id="officer{{ $officer->id }}">
-          <div class="card-content">
-            <div class="row">
-              <div class="col s12 m12 l3">
-                @if($officer->picture == "")
-                  <img src="{{ URL::asset('img/default.png') }}" alt="" class="circle responsive-img">
-                @else
-                  <img src="{{ url('/storage/img/'.$officer->picture) }}" alt="" class="circle responsive-img">
-                @endif
-              </div>
-              <div class="col s12 m12 l9">
-                <span class="card-title black-text">{{ $officer->firstname }} {{ $officer->lastname }} | @if($officer->advisor == 1) Advisor @else {{ $officer->title }} @endif</span>
-                <br />
-                <p>{{ $officer->bio }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        @endforeach
-      </div>
-      <div class="col hide-on-small-only m3 l2">
-        <ul class="section table-of-contents">
-          @foreach($officers as $officer)
-          <li><a href="#officer{{ $officer->id }}">{{ $officer->firstname }} {{ $officer->lastname }}</a></li>
-          @endforeach
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
+@section('head')
+<link href="{{ URL::asset('css/officers.css') }}" type="text/css" rel="stylesheet" media="screen,projection" />
+
 @endsection
+@section('content')
+
+
+
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="brand-hover navbar-brand page-scroll" href="{{ url('/') }}">K-State ACM</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav-hover nav navbar-nav navbar-right">
+                <li>
+                    <a class="page-scroll" href="#officers">Officers</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#jofficers">Junior Officers</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="{{ url('/apps') }}">Dashboard</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+
+<header>
+    <div class="header-content">
+        <div class="header-content-inner">
+            <h1 id="homeHeading">K-State ACM Officers<br> and Junior Officers</h1>
+            <hr class="purple-hr">
+            <p>The Association for Computing Machinery is the professional organization for computer scientists. We strive to bring the computer science
+                community closer by hosting a variety of events as well as getting involved in the community.</p>
+                <a href="#officers" class="btn-purple btn-hover btn btn-primary btn-xl page-scroll">Meet Us</a>
+            </div>
+        </div>
+    </header>
+
+    <section id="officers">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Officers</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                @foreach($officers as $officer)
+
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="thumbnail">
+                        @if($officer->picture == "")
+                          <a href="#" data-toggle="modal" data-target="#modal-{{$officer->id}}"><img src="{{ URL::asset('img/default.png') }}" alt="" class="img-rounded"></a>
+                        @else
+                          <a href="#" data-toggle="modal" data-target="#modal-{{$officer->id}}"><img src="{{ url('/storage/img/'.$officer->picture) }}" alt="" class="img-rounded img-responsive"></a>
+                        @endif
+                        <div class="caption">
+                            <h3>{{ $officer->firstname }} {{ $officer->lastname }}</h3>
+                            <p>@if($officer->advisor == 1) Advisor @else {{ $officer->title }} @endif</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="modal-{{$officer->id }}" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <div class="text-center">
+                                    @if($officer->picture == "")
+                                      <img src="{{ URL::asset('img/default.png') }}" alt="" class="img-rounded">
+                                    @else
+                                      <img src="{{ url('/storage/img/'.$officer->picture) }}" alt="" class="img-rounded img-responsive">
+                                    @endif
+                                    <h3 class="modal-title">{{ $officer->firstname }} {{ $officer->lastname }}</h4>
+                                    </div>
+                                </div>
+                                <div class="modal-body">
+                                    <p>{{ $officer->bio }}</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    @endforeach
+
+                </div>
+            </div>
+        </section>
+
+        <section id="jofficers">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading">Junior Officers</h2>
+                        <hr class="primary">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    @foreach($officers as $officer)
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="thumbnail">
+                            @if($officer->picture == "")
+                              <a href="#" data-toggle="modal" data-target="#modal-{{$officer->id}}"><img src="{{ URL::asset('img/default.png') }}" alt="" class="img-rounded"></a>
+                            @else
+                              <a href="#" data-toggle="modal" data-target="#modal-{{$officer->id}}"><img src="{{ url('/storage/img/'.$officer->picture) }}" alt="" class="img-rounded img-responsive"></a>
+                            @endif
+                            <div class="caption">
+                                <h3>{{ $officer->firstname }} {{ $officer->lastname }}</h3>
+                                <p>@if($officer->advisor == 1) Advisor @else {{ $officer->title }} @endif</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="modal-{{$officer->id }}" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <div class="text-center">
+                                        @if($officer->picture == "")
+                                          <img src="{{ URL::asset('img/default.png') }}" alt="" class="img-rounded">
+                                        @else
+                                          <img src="{{ url('/storage/img/'.$officer->picture) }}" alt="" class="img-rounded img-responsive">
+                                        @endif
+                                        <h3 class="modal-title">{{ $officer->firstname }} {{ $officer->lastname }}</h4>
+                                        </div>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>{{ $officer->bio }}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        @endforeach
+
+                    </div>
+                </div>
+            </section>
+
+
+        @endsection
